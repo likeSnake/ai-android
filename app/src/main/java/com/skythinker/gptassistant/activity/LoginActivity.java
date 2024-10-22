43701a0dd57fdedd403f0f50cba578480d388de2
likeSnake <2843586907@qq.com>
2024-10-22 22:56:49 +0800

    注册界面

--- app/src/main/java/com/skythinker/gptassistant/activity/LoginActivity.java
+++ app/src/main/java/com/skythinker/gptassistant/activity/LoginActivity.java
@@ -1,16 +1,36 @@
 package com.skythinker.gptassistant.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.skythinker.gptassistant.R;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.register)
+    TextView register;
+    @BindView(R.id.start_login)
+    Button start_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+    }
+
+    @OnClick({R.id.register})
+    public void myOnClick(View view){
+        switch (view.getId()){
+            case R.id.register:
+                break;
+
+        }
     }
 }
---
+++ app/src/main/java/com/skythinker/gptassistant/activity/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.skythinker.gptassistant.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.skythinker.gptassistant.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
