7c7300c1696e7e03390e57ba3804e8a794d70000
jiangchunlong <jiangchunlong@51creation.com>
2025-03-03 17:20:45 +0800

    漏传

---
+++ app/src/main/java/com/skythinker/gptassistant/fragment/HomeFragment.java
@@ -0,0 +1,70 @@
+package com.skythinker.gptassistant.fragment;
+
+
+import static com.skythinker.gptassistant.util.MyUtil.APP_USER_GET_TEMPLATE;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.skythinker.gptassistant.R;
+import com.skythinker.gptassistant.entity.base.BaseListEntity;
+import com.skythinker.gptassistant.entity.copyWriter.TextTemplate;
+import com.skythinker.gptassistant.util.HttpUtils;
+import com.skythinker.gptassistant.util.MyToastUtil;
+import com.skythinker.gptassistant.util.MyUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class HomeFragment extends Fragment implements View.OnClickListener {
+
+    private Unbinder unbinder;
+    private List<TextTemplate> data;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+        initView(rootView);
+        initData();
+        initLinsenner();
+        return rootView;
+    }
+
+    public void initData(){
+
+    }
+    public void initView(View rootView){
+
+    }
+
+    public void initLinsenner(){
+
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+    @Override
+    public void onClick(View view) {
+
+    }
+}
